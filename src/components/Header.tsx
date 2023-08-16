2631a913278bdfe1303491b6e1f896ea5432251b
north-tower <muneshmyke@gmail.com>
2023-08-16 23:02:36 +0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ function Header({}: Props) {
 
         <hr  className='mt-2' />
 
-        <section className=''>
+        <section className='flex items-center space-x-2 py-5'>
           <div className='h-16 w-16 sm:w-28 md:w-44 cursor-pointer 
           flex-shrink-0'>
             <Link href='/'>
@@ -60,6 +60,27 @@ function Header({}: Props) {
               height={100} />
             </Link>
           </div>
+
+          <button className='hidden lg:flex items-center space-x-2 w-20'>
+            <p className='text-gray-600 text-sm'>Shop By Category</p>
+            <ChevronDownIcon className='h-4 flex-shrink-0' />
+          </button>
+
+          <div className='flex items-center space-x-2 px-2 md:px-5 py-2 border-black border-2 flex-1'>
+            <MagnifyingGlassIcon className='w-5 text-gray-400'/>
+            <input className='flex-1 outline-none'
+             placeholder="Search for Anything" type='text' />
+          </div>
+
+          <button className='hidden sm:inline bg-blue-600 text-white px-5 
+          md:px-10 py-2 border-2 border-blue-600'>Search</button>
+
+          <Link href="/create">
+          <button className='border-2 border-blue-600 px-5 md:px-10 py-2
+           text-blue-600 hover:bg-blue-600/50 hover:text-white 
+           cursor-pointer'>List Item</button>
+
+          </Link>
         </section>
     </div>
   )
