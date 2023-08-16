f502b2e15dd584ea9c1703b5666f453ace33553d
north-tower <muneshmyke@gmail.com>
2023-08-16 22:40:46 +0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAddress, useDisconnect, useMetamask} from "@thirdweb-dev/react"
 import Link from 'next/link';
 import { BellIcon, ShoppingCartIcon, ChevronDownIcon, MagnifyingGlassIcon,} from "@heroicons/react/24/outline"
+import Image from 'next/image';
 
 type Props = {}
 
@@ -11,7 +12,7 @@ function Header({}: Props) {
   const address = useAddress();
   
   return (
-    <div>
+    <div className='max-w-6xl mx-auto p-2'>
         <nav className='flex justify-between'>
             <div className='flex items-center space-x-2 text-sm'>
               {address ? (
@@ -19,8 +20,7 @@ function Header({}: Props) {
                  className="connectWalletBtn">Hi, {address.slice(0,5) +
                  "..." + address.slice(-4)}</button>
               ) : (
-                <button onClick={connectWithMetamask}
-                className="connectWalletBtn">Connect your wallet</button>
+                <button onClick={connectWithMetamask} className="connectWalletBtn">Connect your wallet</button>
               )}
 
               <p className='hidden md:inline-flex cursor-pointer'>
@@ -36,11 +36,31 @@ function Header({}: Props) {
               <p className='hidden md:inline-flex cursor-pointer'>Watchlist</p>
           
 
-                <Link href="/addItem">Add to inventory
-                  <ChevronDownIcon className='h-4' />
-                </Link>
+              <Link href="/addItem" className='flex items-center hover:link'>
+                Add to inventory
+                <ChevronDownIcon className='h-4' />
+              </Link>
+
+              <BellIcon className='h-6 w-6' />
+              <ShoppingCartIcon className='h-6 w-6' />
+
             </div>
         </nav>
+
+        <hr  className='mt-2' />
+
+        <section className=''>
+          <div className='h-16 w-16 sm:w-28 md:w-44 cursor-pointer 
+          flex-shrink-0'>
+            <Link href='/'>
+              <Image alt="Xbay Logo" 
+              className='h-full w-full object-contain'
+              src="https://links.papareact.com/bdb"
+              width={100}
+              height={100} />
+            </Link>
+          </div>
+        </section>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <div >
-      Lets Build 
+      
       <Header />
    </div>
   )
