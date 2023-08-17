bee257f702dd11fe0632b7be14b3cc48a72f7529
north-tower <muneshmyke@gmail.com>
2023-08-17 14:42:19 +0300

    contract

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
-import { useActiveListings, useContract, MediaRenderer } from '@thirdweb-dev/react'
+import { useActiveListings, useContract, MediaRenderer, ListingType } from '@thirdweb-dev/react'
+import { BanknotesIcon, ClockIcon } from '@heroicons/react/24/outline'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,16 +17,16 @@ export default function Home() {
    <div >
       <Header />
 
-      <main>
+      <main className='max-w-6xl mx-auto py-2 px-6'>
         {loadingListings ? 
         <p className='text-center animate-pulse text-blue-500'>
           Loading Listings</p> :
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
-          lg:grid-cols-4 gap-5 mx-auto '>
+          <div className='grid grid-cols-1 sm:grid-cols-2 
+          md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto '>
             {listings?.map(listing => (
               <div className='flex flex-col card hover:scale-105
               transition-all duration-150 ease-out' key={listing.id}>
-                <div className='flex-1 flex-col pb-2 items-center'>
+                <div className='flex-1 flex flex-col pb-2 items-center'>
                   <MediaRenderer className='w-44'
                    src={listing.asset.image} />
                 </div>
@@ -42,11 +43,25 @@ export default function Home() {
                     </div>
 
                 <p>
-                  <span className='font-bold'>
+                  <span className='font-bold mr-1 '>
                     {listing.buyoutCurrencyValuePerToken.displayValue}
                   </span>
                   {listing.buyoutCurrencyValuePerToken.symbol}
                 </p>
+
+                <div className={`flex items-center space-x-1 justify-end text-xs border w-fit
+                ml-auto p-2 rounded-lg text-white ${listing.type === ListingType.Direct
+                ? "bg-blue-500" : "bg-red-500"}`}>
+                  <p>
+                    {listing.type === ListingType.Direct ? "Buy Now" : "Auction"}
+                  </p>
+                  {listing.type === ListingType.Direct ? (
+                    <BanknotesIcon className='h-4' />
+                  ) : (
+                    <ClockIcon className='h-4' />
+                  )}
+
+                </div>
                 </div>
               </div>
             ))}
