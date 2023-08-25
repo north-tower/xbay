a891e9393a71e39b65fbfe311159f75d12999607
north-tower <muneshmyke@gmail.com>
2023-08-25 18:25:11 +0300

    tex

---
+++ src/pages/tex.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+function tex({}: Props) {
+
+  return (
+    <div  className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed" style={{ backgroundImage: "url('header.png')" }}>
+        hhhh
+    </div>
+  )
+}
+
+export default tex
