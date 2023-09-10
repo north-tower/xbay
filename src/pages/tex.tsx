6c10d4c680442c8a70c1a3b08a3f5206818c5168
north-tower <muneshmyke@gmail.com>
2023-09-10 22:26:46 +0300

    miki1

--- src/pages/tex.tsx
+++ src/pages/tex.tsx
@@ -5,11 +5,11 @@ type Props = {}
 function tex({}: Props) {
 
   return (
-    <div  classNameName="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed" style={{ backgroundImage: "url('header.png')" }}>
+    <div  className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed" style={{ backgroundImage: "url('header.png')" }}>
           <div className="h-full">
             </div>
    
-      <div class="w-full md:w-3/5 lg:md:3/5 container mx-auto">
+      <div className="w-full md:w-3/5 lg:md:3/5 container mx-auto">
         <div className="w-full flex items-center justify-between">
           <a className="flex items-center text-yellow-400 no-underline hover:no-underline font-bold text-4xl m-2 lg:text-4xl" href="#">
             Texlar<span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 via-pink-500 to-yellow-600">Mtaani</span>
