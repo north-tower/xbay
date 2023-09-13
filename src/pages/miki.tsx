b576fd741e81bb96cd76193728e77b17554f527b
north-tower <muneshmyke@gmail.com>
2023-09-13 21:37:13 +0300

    moki

--- src/pages/miki.tsx
+++ src/pages/miki.tsx
@@ -5,7 +5,12 @@ type Props = {}
 function miki({}: Props) {
   return (
     <div  className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed" style={{ backgroundImage: "url('header.png')" }}>
-      
+       
+       <div className="w-full md:w-3/5 lg:md:3/5 container mx-auto">
+        <div className="w-full flex items-center justify-between">
+          <a className="flex items-center text-yellow-400 no-underline hover:no-underline font-bold text-4xl m-2 lg:text-4xl" href="#">
+            Texlar<span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 via-pink-500 to-yellow-600">Mtaani</span>
+          </a>
       miki</div>
   )
 }
