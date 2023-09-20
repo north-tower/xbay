7e70f135aec26ec60e13a5dccf3ca966f61d5113
Miki <muneshmyke@gmail.com>
2023-09-20 22:41:00 +0300

    Update create.tsx

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -14,6 +14,10 @@ import { useAddress, useContract, MediaRenderer, useNetwork, useNetworkMismatch,
 
 type Props = {}
 
+// Import necessary hooks and components
+
+// ... (existing code) ...
+
 function Create({}: Props) {
     const address = useAddress();
     const { contract } = useContract(
@@ -23,42 +27,59 @@ function Create({}: Props) {
         process.env.NEXT_PUBLIC_COLLECTION_CONTRACT,"nft-collection"
     );
     const ownedNfts = useOwnedNFTs(collectionContract,address);
-   
-  return (
-    <div>
-        <Header />
+    const createAuctionListing = useCreateAuctionListing(contract);
+    const createDirectListing = useCreateDirectListing(contract);
 
-        <main className='max-w-6xl mx-auto p-10 pt-2'>
-            <h1 className='text-4xl font-bold'>List an Item</h1>
-            <h2 className='text-xl font-semibold pt-5'>
-                Select an Item you would like to Sell</h2>
-            <hr className='mb-5' />
-            <p>Below you will find the NFTs you own in your wallet</p>
-        
-        <div className='flex overflow-x-scroll space-x-2 p-4'>
-            {ownedNfts?.data?.map((nft) => (
-                <div key={nft.metadata.id} className='flex flex-col 
-                space-y-2 card min-w-fit border-2 bg-green-50'>
-                    <MediaRenderer className='h-48 rounded-lg'
-                    src={nft.metadata.image} />
-                <p className='text-lg truncate font-bold'>
-                    {nft.metadata.name}</p>
-                <p className='text-xs truncate'>
-                    {nft.metadata.description}</p>
+    // Function to list NFT in marketplace
+    const listNFT = async (nftId, price, isAuction) => {
+        try {
+            if(isAuction) {
+                await createAuctionListing(nftId, price);
+            } else {
+                await createDirectListing(nftId, price);
+            }
+            // Handle success (e.g., show a success message)
+        } catch (error) {
+            // Handle error (e.g., show an error message)
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <Header />
+
+            <main className='max-w-6xl mx-auto p-10 pt-2'>
+                {/* ... (existing code) ... */}
+
+                <div className='flex overflow-x-scroll space-x-2 p-4'>
+                    {ownedNfts?.data?.map((nft) => (
+                        <div key={nft.metadata.id} className='flex flex-col 
+                            space-y-2 card min-w-fit border-2 bg-green-50'>
+                            <MediaRenderer className='h-48 rounded-lg'
+                                src={nft.metadata.image} />
+                            <p className='text-lg truncate font-bold'>
+                                {nft.metadata.name}</p>
+                            <p className='text-xs truncate'>
+                                {nft.metadata.description}</p>
+
+                            {/* Button to list NFT */}
+                            <button onClick={() => listNFT(nft.metadata.id, 100, true)}>
+                                List in Auction
+                            </button>
+
+                            <button onClick={() => listNFT(nft.metadata.id, 200, false)}>
+                                List Directly
+                            </button>
+                        </div>
+                    ))}
                 </div>
-
-
-
-
-            ))}
+            </main>
         </div>
-        </main>
-    </div>
-  )
+    )
 }
 
 export default Create
-
 
 
 
