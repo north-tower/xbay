5b2f4022090e42cc0472559e85f830696eee673c
north-tower <muneshmyke@gmail.com>
2023-10-06 08:39:18 +0300

    ma

--- src/pages/miki.tsx
+++ src/pages/miki.tsx
@@ -12,8 +12,9 @@ function miki({}: Props) {
       <h1>mikiss</h1>
       <h1>mikiss</h1>
       <h1>mianatures</h1>
-      <h1>mianatures</h1>
-      
+     
+       <h1>mianatures</h1>
+       <h1>mianatures</h1>
 
 
       
