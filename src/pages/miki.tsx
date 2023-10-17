8fc74a736abd7ac3870e7dbeb3c2008020958272
north-tower <muneshmyke@gmail.com>
2023-10-17 08:58:37 +0300

    mz

--- src/pages/miki.tsx
+++ src/pages/miki.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
+import Header from '../components/Header'
+import { useMakeOffer, useContract, Web3Button } from "@thirdweb-dev/react";
+
 
 type Props = {}
 
 function miki({}: Props) {
+  const contractAddress = "0x1f3a4c667bc97899304E4849705C88FB8C334E11";
+  const { contract } = useContract(contractAddress, "marketplace");
+  const { mutateAsync: makeOffer, isLoading, error } = useMakeOffer(contract);
+
   return (
-    <div>miki
-      <h1>mikiss</h1>
-      <h1>mikiss</h1>
-      <h1>mikiss</h1>
-      <h1>mikiss</h1>
-      <h1>mikiss</h1>
-      <h1>mikiss</h1>
-      <h1>mianatures</h1>
-     
-       <h1>mianatures</h1>
-       <h1>mianatures</h1>
-       <h1>mianatures</h1>
-       <h1>mianatures</h1>
-       <h1>mianatures</h1>
-       <h1>mianatures</h1>c
+    <div>
+      <Header />
 
-       <h1>mianatures</h1>c
-
-      
-
-
-
-      
+  
+    <Web3Button
+      contractAddress={contractAddress}
+      action={() =>
+        makeOffer({
+          listingId: 1, // ID of the listing to make an offer on
+          pricePerToken: 0.0000091,  // Price per token to offer (in the listing's currency)
+          quantity: 1, // Number of NFTs you want to buy (used for ERC1155 NFTs)
+        })
+      }
+    >
+      Make Bid
+    </Web3Button>   
       
     </div>
   )
