6232a800c68a1e74e404d8cedfc5bc891b0f6071
north-tower <muneshmyke@gmail.com>
2023-10-31 13:04:36 +0300

    remo3

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import Image from 'next/image'
+import { InformationCircleIcon } from "@heroicons/react/24/outline"
+
 
 type Props = {}
 
 function Project({}: Props) {
   return (
     <div>
-        <section className='flex items-center max-w-6xl  mx-auto p-2 space-x-2 py-5 mt-6 '>
-        
-        <Image src={'https://www.remotasks.com/static/images/collaboration.svg'} alt={'amkk'} width={240} height={240}/>
+        <section className='flex items-center max-w-6xl mx-auto p-2 space-x-2 py-5 mt-6 '>
+          <div className='flex'>
+            <div>
+             <Image src={'https://www.remotasks.com/static/images/collaboration.svg'} alt={'amkk'} width={240} height={240}/>
+            </div>
+          <div className='ml-8 w-full'>
+            <p className=''>Welcome back, John!</p>
+              <div className=' items-center space-x-2 px-2 md:px-5 py-2  bg-gray-100 border-gray-100
+               rounded-md border-2 flex-1 mt-4 w-full'>
+                  <div className="flex items-center">
+                    <span className='font-bold'>  Your Active Projects </span> 
+                    <span className='m-1'><InformationCircleIcon className='h-5'/></span>
+              </div>
+                  No Active Project
+                <button className='bg-blue-500'>Start Task</button>
+              </div>
+            </div>
+          </div>
         </section>
 
+
         <section className='flex items-center max-w-6xl  mx-auto p-2 space-x-2 py-5 '>
-          <div className='flex items-center space-x-2 px-2 md:px-5 py-2 border-gray-100 rounded-md border-2 flex-1'>
-           <p>Progress Tracker
+          <div className='flex items-center space-x-2 px-2 md:px-5 py-2 
+          border-gray-100 rounded-md border-2 flex-1'>
+           <p>Progress Tracker <span></span>
             </p> 
             0 XP
           </div>
