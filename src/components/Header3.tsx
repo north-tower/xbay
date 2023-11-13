ebfc0702fd6ccb4873da124d740cb873e3fe754c
north-tower <muneshmyke@gmail.com>
2023-11-13 11:24:57 +0300

    ra

--- src/components/Header3.tsx
+++ src/components/Header3.tsx
@@ -12,7 +12,7 @@ function Header({}: Props) {
   const address = useAddress();
   
   return (
-    <div className='max-w-7xl mx-auto p-2'>
+    <div className='max-w-6xl mx-auto p-2'>
         <nav className='flex justify-between'>
            
             <div className='flex items-center space-x-4 text-sm'>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,22 +7,33 @@ type Props = {};
 function Project({}: Props) {
   return (
     <div>
-      <section className='flex items-center max-w-6xl mx-auto p-2 space-x-2 py-5 mt-6'>
+      <section className='flex items-center max-w-5xl mx-auto p-2 space-x-2 py-5 mt-6'>
         <div className='flex'>
           <div>
             <Image src={'https://www.remotasks.com/static/images/collaboration.svg'}
-              alt={'amkk'} width={240} height={240} />
+              alt={'amkk'} width={280} height={280} />
           </div>
-          <div className='ml-8 w-full'>
+          <div className='ml-12 w-full'>
             <p className=''>Welcome back, John!</p>
-            <div className='flex flex-row'>
-            <div className=' items-center space-x-2 px-2 md:px-5 py-2  bg-gray-100 border-gray-100
-               rounded-md border-2 flex-1 mt-4 w-full'>
-                  <div className="flex items-center">
-                    <span className='font-bold'>  Your Active Projects </span> 
-                    <span className='m-1'><InformationCircleIcon className='h-5'/></span>
+            <div className='flex flex-row '>
+              <div className='space-x-2 px-2 md:px-5 py-1 bg-gray-50 border-gray-100
+               rounded-md border-1 mt-4 w-full'>
+                <div className="flex items-center">
+                  <span >Your Active Projects </span>
+                  <span className='m-1'><InformationCircleIcon className='h-5'/></span>
+                  
+                </div>
+               <div className="flex items-center justify-between">
+              
+                <span className='pr-36'>No active project</span>  
+              <span className='pl-64'>
+
+              <button className='bg-sky-600 text-white  rounded-md p-2'>Start Tasking </button>
+
+              </span>
               </div>
-            </div>
+              </div>
+
             </div>
           </div>
         </div>
