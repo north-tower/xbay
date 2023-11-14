7e24261940739b28044275ddca67882226f4f6d7
north-tower <muneshmyke@gmail.com>
2023-11-14 13:33:50 +0300

    rg

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CalendarIcon , MinusCircleIcon} from "@heroicons/react/24/solid"
+import { CalendarIcon , MinusCircleIcon, FireIcon} from "@heroicons/react/24/solid"
 
 
 type Props = {}
@@ -37,31 +37,42 @@ function Progress({}: Props) {
                 </div>
             </div>
             <div className="flex-auto w-32 ml-5">
-            <div className='space-x-2 px-2 md:px-5 py-1 border-gray-200 rounded-md border-2 mt-4 w-full'>
-            <h4 className='font-bold pb-2'>
-    Current Status
-  </h4>
-  <div className='bg-gray-200 rounded-md p-2'>
-    <div className="flex flex-col">
-      <div>
-      <div className="flex">
+              <div className='space-x-2 px-2 md:px-5 py-1 border-gray-200 rounded-md border-2 mt-4 w-full'>
+                <h4 className='font-bold pb-2'>
+                Current Status
+              </h4>
+              <div className='bg-gray-200 rounded-md p-2'>
+                <div className="flex flex-row">
+                  <div className="basis-1/2">
+                    <span className='text-gray-500 text-sm'>
+                    Tier
+                  </span>
+              <div className="flex items-center">
+                <span className="">
+                <MinusCircleIcon className="h-4 w-4 text-gray-600" />
+                </span>
+                <span className='text-sm'>Standard</span>
+              </div>
+              </div>
+              <div className="basis-1/2">
+     
+              <div className="flex ">
+                <span className="">
+                  <FireIcon className="h-4 w-4 text-amber-400" />
+                </span>
+                <span className='text-sm text-gray-500'>4 Remopoints to be promoted</span>
+              </div>
+        
       
-      <div className="flex-1 w-64 ...">
-      <span className='text-gray-500 text-sm'>
-      Tier
-    </span>
       </div>
-      <div className="flex-1 w-32 text-gray-400 text-xs">
-      <p> 4 Remopoints to be promoted
-        </p> 
-      </div>
-    </div>
 
-
-      </div>
-      <div>02</div>
+      
   
-    </div>
+</div>
+
+
+  
+ 
     
    </div>
    </div>
